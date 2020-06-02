bdbd5d1e5ba1e21aa237d0fbe597f8a9f6f09dee
Abbe91 <abdullah.abo.alhaijja@hotmail.com>
2020-06-02 22:58:12 +0200

    card input

--- my-app/src/components/Content.tsx
+++ my-app/src/components/Content.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
 import { Layout } from 'antd';
-import Products from './Products'
-import ProductList from './ProductList'
+import Products from './Products';
+import ProductList from './ProductList';
+// import Form from './CardForm';
 const { Content } = Layout;
 
 function content(){
     return(
         <Content>Content
 
-            <div style={{width:1000, height:500, backgroundColor:"white"}}>
+            <div style={{width:10000, height:500, backgroundColor:"white"}}>
                 <ProductList 
                     products = {Products}
                 />
+            </div>
+            <div>
+                
             </div>
         </Content>
   );
