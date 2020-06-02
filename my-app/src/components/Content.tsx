6371a20d8e36286b4d874a74d78e89ff483bf45d
Abbe91 <abdullah.abo.alhaijja@hotmail.com>
2020-06-03 00:21:42 +0200

    text to the main page

--- my-app/src/components/Content.tsx
+++ my-app/src/components/Content.tsx
@@ -8,9 +8,13 @@ const { Content } = Layout;
 
 function content(){
     return(
-        <Content>Content
+        <Content>
+            <h2>We have been in the business for quite a while now, 
+                and it that time we have not only managed to make close relationships with numerous suppliers all over the world, 
+                but also to recognize what people need. This means that we are always able to offer all the latest phones, 
+                great prices, reliable service, fast delivery and premium customer support.</h2>
 
-            <div style={{width:10000, height:500, backgroundColor:"white"}}>
+            <div style={{ backgroundColor:"white"}}>
                 <ProductList 
                     products = {Products}
                 />
