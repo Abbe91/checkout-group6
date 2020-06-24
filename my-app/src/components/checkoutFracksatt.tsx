5b479f691edfc68e78f055e8290f35da48772317
Anukhatra <anu.khatra@gmail.com>
2020-06-24 09:34:19 +0200

    commit

--- my-app/src/components/checkoutFracksatt.tsx
+++ my-app/src/components/checkoutFracksatt.tsx
@@ -19,10 +19,10 @@ export interface Shipping {
 
 export const shippingAlternatives: Shipping[] = [
     {
-        id: 1,
+        id: 3,
         name: 'Postnord',
-        speed: 3,
-        cost: 50
+        speed: 1,
+        cost: 150
     },
     {
         id: 2,
@@ -31,18 +31,27 @@ export const shippingAlternatives: Shipping[] = [
         cost: 100
     },
     {
-        id: 3,
+        id: 1,
         name: 'Express Post',
-        speed: 1,
-        cost: 150
+        speed: 3,
+        cost: 50
     },
 ];
 
 
 interface Props {}
+interface State{}
 
-class CheckoutFracksatt extends Component<Props> {
+class CheckoutFracksatt extends Component<Props,State> {
 
+    constructor(props: Props) {    
+        super(props);
+        this.state = {
+           
+
+            
+        };
+    }
 
     render() {
         return (
@@ -52,23 +61,26 @@ class CheckoutFracksatt extends Component<Props> {
                         <br />
                         <Title level={4}>Freight Options</Title>
                         {shippingAlternatives.map((shipping) => {
+                 
+                           if (shipping.cost) {   
                             return (
                                 <Space direction="horizontal">
-                                    <Card key={shipping.id} title={shipping.name}
+                                    <Card hoverable key={shipping.id} title={shipping.name}
                                         style={{ marginRight: '30px', borderRadius: '15px' }} actions={[]} >
                                         <Text type="warning">Time for delivery:{shipping.speed * 24} hours</Text><br /><br />
-                                        <Text type="warning">Shpping Cost:{shipping.cost} kr</Text><br /><br />
+                                        <Text type="warning">Shipping Cost:{shipping.cost} kr</Text><br /><br />
                                         <Text type="warning">Delivery date:</Text>
                                         <Meta description={new Date(new Date().setDate(new Date().getDate() + shipping.speed)).toISOString().substring(0, 10)} />
                                         <br />
-
-
-                                        <Button onClick={() => setSelectedShipping(shipping)}>Select Shipping</Button>
+                                        
+{/*                                         <Button onClick={() => setSelectedShipping(shipping)}>Select Shipping</Button> */}
+                                        <button autoFocus style={{backgroundColor:"white", border:"#1E90FF solid 2px"}} onClick={() => setSelectedShipping(shipping)}>Select Shipping</button>
 
                                     </Card>
                                 </Space>
-                            )
+                            ) }
                         })}
+                        
                         <div><h4>Shipping cost selected: {selectedShipping.cost}</h4></div>
                     </div>
                 )}
